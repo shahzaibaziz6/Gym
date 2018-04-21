463fb03fbbeb4c13322fd19b86c0649e71897ad1
shahzaibaziz6 <shahzaibaziz6@gmail.com>
2018-04-21 13:21:11 +0500

    Initial

--- app/src/main/java/com/shahzaibaziz/gym/HomeActivity.java
+++ app/src/main/java/com/shahzaibaziz/gym/HomeActivity.java
@@ -1,13 +1,55 @@
 package com.shahzaibaziz.gym;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener
+{
 
+    TextView tvNewEntry,tvSearchEntry,tvDeleteEntry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvDeleteEntry= findViewById(R.id.tv_home_delete_entry);
+        tvNewEntry= findViewById(R.id.tv_home_new_entry);
+        tvSearchEntry= findViewById(R.id.tv_home_search_entry);
+        tvSearchEntry.setOnClickListener(this);
+        tvDeleteEntry.setOnClickListener(this);
+        tvNewEntry.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent =null;
+        switch (v.getId())
+        {
+            case(R.id.tv_home_new_entry):
+            {
+                intent= new Intent(this,NewEntry.class);
+                break;
+            }
+
+            case(R.id.tv_home_search_entry):
+            {
+                intent= new Intent(this,SearchActivity.class);
+                break;
+            }
+
+            case(R.id.tv_home_delete_entry):
+            {
+                intent= new Intent(this,DeleteActivity.class);
+                break;
+            }
+        }
+        if(intent!=null)
+        {
+            startActivity(intent);
+        }
+
     }
 }
